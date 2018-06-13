a6d6996668293b589d852fbb66c5ef9ae3bd1046
samarasallam <39006846+samarasallam@users.noreply.github.com>
2018-06-13 19:50:26 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-
-var numberofDays = prompt("Enter Date to meet:");
-var newDate = new Date(Date.now()+numberofDays*24*60*60*1000);
-
-document.write("Today is:", new Date());
-document.write('</em><br/>');
-document.write("We are meeting on:",newDate);
